c980a43c2f9aa039baaaaffbffda4135abefa872
Sunderhus <matheus.sunderhus@gmail.com>
2023-09-24 14:01:02 -0300

    feat: add shadcn button with dark theme preset

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-
-export function App(){
-  return(
-    <p> hello world</p>
-  )
-}
---
+++ src/presentation/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/presentation/components/ui/button"
+ 
+export function App() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
